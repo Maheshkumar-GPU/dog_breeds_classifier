5435b259880e0e50bf2a0214020a583faef6ed3c
Maheshkumar <tigermahesh6385@gmail.com>
2026-03-21 20:46:49 +0530

    configuration file was created for easy for src file

---
+++ config.py
@@ -0,0 +1,8 @@
+dataset_path =r"D:\ml project datasets\dog_breed_classifer\Dog Breeds Image Dataset"
+IMAGE_SIZE = (224,224)
+BATCH_SIZE = 16
+EPO =15
+LEARNING_RATE  = 0.0001
+model_path = 'outputs/model1.h5'
+label_map_path = 'outputs/label_map_path.json'
+results_path = 'outputs/plot.png'
